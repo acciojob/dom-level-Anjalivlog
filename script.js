a6f2456f514fa51bc921ea3aad520b0cf3103dda
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 17:31:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function(){
+    let element = document.getElementById('level');
+    let counter = 0;
+
+    while (element) {
+        element = element.parentNode;
+        counter++;
+    }
+
+    alert('The level of the element is: ' + counter);
+});
